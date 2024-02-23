a1845c7eb4d876b72a4fde76e60debb0f2dcb750
Sudip <112951953+Sudip7878@users.noreply.github.com>
2024-02-23 13:00:57 +0545

    commnetd

---
+++ NccLabSudip/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+interface animal
+{
+    void makesound();
+    void eat();
+}
+
+
+interface car
+{
+    void modelname();
+    void price();
+}
+class cow : animal, car
+{
+    public void makesound() {
+        Console.WriteLine("moo moo");
+    }
+    public void eat() {
+        Console.WriteLine("eat grass");
+    }
+
+    public void modelname()
+    {
+        Console.WriteLine("bmw");
+    }
+    public void price() { Console.WriteLine("2000"); }
+}
+class program
+{
+    static void Main(string[] args)
+    {
+        cow cow = new cow();
+        cow.makesound();
+        cow.price();
+        Console.ReadLine();
+    }
+}
